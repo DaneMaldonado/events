d2ea4508860c78c397ca28455cbe9c9083d376b2
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-03-11 11:23:42 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,25 @@
+def on_a_pressed():
+    pass
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+def say_something(sprite: Sprite):
+    sprite.say_text("Hello my name is Dane.")
+my_sprite = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . 4 4 4 . . . . 4 4 4 . . . . 
+            . 4 5 5 5 e . . e 5 5 5 4 . . . 
+            4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+            4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+            e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+            . e e 5 5 5 5 5 5 5 5 e e . . . 
+            . . e 5 f 5 5 5 5 f 5 e . . . . 
+            . . f 5 5 5 4 4 5 5 5 f . f f . 
+            . . . 4 5 5 f f 5 5 6 f f 5 f . 
+            . . . f 6 6 6 6 6 6 4 4 4 5 f . 
+            . . . f 5 5 5 5 5 5 5 f f f . . 
+            . . . f 5 4 5 f f f 5 f . . . . 
+            . . . f f f f f . . f f . . . .
+    """),
+    SpriteKind.player)
+controller.move_sprite(my_sprite)
+my_sprite.set_stay_in_screen(True)
---
+++ main.ts
@@ -0,0 +1,25 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    my_sprite.sayText("Hello my name is Dane.")
+})
+function say_something (sprite: Sprite) {
+    sprite.sayText("Hello my name is Dane.")
+}
+let my_sprite: Sprite = null
+my_sprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . f f . 
+    . . . 4 5 5 f f 5 5 6 f f 5 f . 
+    . . . f 6 6 6 6 6 6 4 4 4 5 f . 
+    . . . f 5 5 5 5 5 5 5 f f f . . 
+    . . . f 5 4 5 f f f 5 f . . . . 
+    . . . f f f f f . . f f . . . . 
+    `, SpriteKind.Player)
+my_sprite.setStayInScreen(true)
+say_something(my_sprite)
