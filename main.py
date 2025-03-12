c8788ee3ef8d8873c5140177c3d8f6ce9b0ed00c
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-03-11 21:29:12 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,80 @@
-def on_a_pressed():
-    say_something(my_sprite)
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
-
 def say_something(sprite: Sprite):
     sprite.say_text("Hello my name is Dane.")
-my_sprite: Sprite = None
-my_sprite = sprites.create(img("""
+
+def on_countdown_end():
+    game.game_over(True)
+info.on_countdown_end(on_countdown_end)
+
+def on_on_overlap(sprite2, otherSprite):
+    OverlapFunction()
+    info.change_life_by(1)
+    music.play(music.create_sound_effect(WaveShape.SINE,
+            200,
+            600,
+            255,
+            0,
+            150,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        music.PlaybackMode.UNTIL_DONE)
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap)
+
+def on_on_score():
+    game.game_over(True)
+info.on_score(100, on_on_score)
+
+def on_on_score2():
+    game.game_over(False)
+info.on_score(0, on_on_score2)
+
+def on_on_overlap2(sprite3, otherSprite2):
+    EnemyChaserPlayer.set_position(randint(10, 150), randint(10, 110))
+    OverlapFunction()
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
+def on_b_released():
+    say_something(Duck)
+controller.B.on_event(ControllerButtonEvent.RELEASED, on_b_released)
+
+def EnemyChaser():
+    global EnemyChaserPlayer
+    EnemyChaserPlayer = sprites.create(img("""
+            ........................
+                    ....ffffff..............
+                    ..ffeeeef2f.............
+                    .ffeeeef222f............
+                    .feeeffeeeef...cc.......
+                    .ffffee2222ef.cdc.......
+                    .fe222ffffe2fcddc.......
+                    fffffffeeeffcddc........
+                    ffe44ebf44ecddc.........
+                    fee4d41fddecdc..........
+                    .feee4dddedccc..........
+                    ..ffee44e4dde...........
+                    ...f222244ee............
+                    ...f2222e2f.............
+                    ...f444455f.............
+                    ....ffffff..............
+                    .....fff................
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+        """),
+        SpriteKind.enemy)
+    EnemyChaserPlayer.set_position(2, 2)
+    EnemyChaserPlayer.follow(Duck, 88)
+def OverlapFunction():
+    food.set_position(randint(10, 150), randint(10, 110))
+    info.change_score_by(1)
+EnemyChaserPlayer: Sprite = None
+food: Sprite = None
+Duck: Sprite = None
+Duck = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . 4 4 4 . . . . 4 4 4 . . . . 
             . 4 5 5 5 e . . e 5 5 5 4 . . . 
@@ -22,28 +91,29 @@ my_sprite = sprites.create(img("""
             . . . f f f f f . . f f . . . .
     """),
     SpriteKind.player)
-my_sprite.set_stay_in_screen(True)
-
-
-# Create the food sprite and place it at a random location
 food = sprites.create(img("""
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-"""), SpriteKind.food)
+        . . . . . . . 6 . . . . . . . . 
+            . . . . . . 8 6 6 . . . 6 8 . . 
+            . . . e e e 8 8 6 6 . 6 7 8 . . 
+            . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+            . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+            . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+            e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+            e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+            e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+            e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+            e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+            e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+            e e 2 e 2 2 4 2 2 e e e c . . . 
+            e e e e 2 e 2 2 e e e c . . . . 
+            e e e 2 e e c e c c c . . . . . 
+            . c c c c c c c . . . . . . . .
+    """),
+    SpriteKind.food)
+Duck.follow(food)
+Duck.set_stay_in_screen(True)
 food.set_position(randint(10, 150), randint(10, 110))
-
-# Handle collision between player and food
-def on_overlap(sprite, otherSprite):
-    # Actions to take when the player collides with food
-    food.set_position(randint(10, 150), randint(10, 110))  # Move food to a new random location
-    info.change_score_by(1)  # Increment the score by 1
-
-# When the player collides with the food, run the on_overlap function
-sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_overlap)
-
-def on_update():
-    pass
-game.on_update(on_update)
+info.set_score(0)
+info.set_life(3)
+EnemyChaser()
+info.start_countdown(40)
--- main.ts
+++ main.ts
@@ -1,73 +1,107 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    say_something(my_sprite)
-})
-function say_something (sprite: Sprite) {
+function say_something(sprite: Sprite) {
     sprite.sayText("Hello my name is Dane.")
 }
-// Handle collision between player and food
-// Increment the score by 1
-// When the player collides with the food, run the on_overlap function
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    food = sprites.create(img`
-        . . . . . . . 6 . . . . . . . . 
-        . . . . . . 8 6 6 . . . 6 8 . . 
-        . . . e e e 8 8 6 6 . 6 7 8 . . 
-        . . e 2 2 2 2 e 8 6 6 7 6 . . . 
-        . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
-        . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
-        e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
-        e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
-        e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
-        e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
-        e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
-        e 2 2 2 2 2 2 2 4 e 2 e e c . . 
-        e e 2 e 2 2 4 2 2 e e e c . . . 
-        e e e e 2 e 2 2 e e e c . . . . 
-        e e e 2 e e c e c c c . . . . . 
-        . c c c c c c c . . . . . . . . 
-        `, SpriteKind.Player)
-    // Actions to take when the player collides with food
-    food.setPosition(randint(10, 150), randint(10, 110))
-    // Move food to a new random location
-    info.changeScoreBy(1)
+
+info.onCountdownEnd(function on_countdown_end() {
+    game.gameOver(true)
 })
-let food: Sprite = null
-let my_sprite: Sprite = null
-my_sprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . 4 4 4 . . . . 4 4 4 . . . . 
-    . 4 5 5 5 e . . e 5 5 5 4 . . . 
-    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
-    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
-    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
-    . e e 5 5 5 5 5 5 5 5 e e . . . 
-    . . e 5 f 5 5 5 5 f 5 e . . . . 
-    . . f 5 5 5 4 4 5 5 5 f . f f . 
-    . . . 4 5 5 f f 5 5 6 f f 5 f . 
-    . . . f 6 6 6 6 6 6 4 4 4 5 f . 
-    . . . f 5 5 5 5 5 5 5 f f f . . 
-    . . . f 5 4 5 f f f 5 f . . . . 
-    . . . f f f f f . . f f . . . . 
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function on_on_overlap(sprite2: Sprite, otherSprite: Sprite) {
+    OverlapFunction()
+    info.changeLifeBy(1)
+    music.play(music.createSoundEffect(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
+info.onScore(100, function on_on_score() {
+    game.gameOver(true)
+})
+info.onScore(0, function on_on_score2() {
+    game.gameOver(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_on_overlap2(sprite3: Sprite, otherSprite2: Sprite) {
+    EnemyChaserPlayer.setPosition(randint(10, 150), randint(10, 110))
+    OverlapFunction()
+    info.changeLifeBy(-1)
+})
+controller.B.onEvent(ControllerButtonEvent.Released, function on_b_released() {
+    say_something(Duck)
+})
+function EnemyChaser() {
+    
+    EnemyChaserPlayer = sprites.create(img`
+            ........................
+                    ....ffffff..............
+                    ..ffeeeef2f.............
+                    .ffeeeef222f............
+                    .feeeffeeeef...cc.......
+                    .ffffee2222ef.cdc.......
+                    .fe222ffffe2fcddc.......
+                    fffffffeeeffcddc........
+                    ffe44ebf44ecddc.........
+                    fee4d41fddecdc..........
+                    .feee4dddedccc..........
+                    ..ffee44e4dde...........
+                    ...f222244ee............
+                    ...f2222e2f.............
+                    ...f444455f.............
+                    ....ffffff..............
+                    .....fff................
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+        `, SpriteKind.Enemy)
+    EnemyChaserPlayer.setPosition(2, 2)
+    EnemyChaserPlayer.follow(Duck, 88)
+}
+
+function OverlapFunction() {
+    food.setPosition(randint(10, 150), randint(10, 110))
+    info.changeScoreBy(1)
+}
+
+let EnemyChaserPlayer : Sprite = null
+let food : Sprite = null
+let Duck : Sprite = null
+Duck = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+            . . 4 4 4 . . . . 4 4 4 . . . . 
+            . 4 5 5 5 e . . e 5 5 5 4 . . . 
+            4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+            4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+            e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+            . e e 5 5 5 5 5 5 5 5 e e . . . 
+            . . e 5 f 5 5 5 5 f 5 e . . . . 
+            . . f 5 5 5 4 4 5 5 5 f . f f . 
+            . . . 4 5 5 f f 5 5 6 f f 5 f . 
+            . . . f 6 6 6 6 6 6 4 4 4 5 f . 
+            . . . f 5 5 5 5 5 5 5 f f f . . 
+            . . . f 5 4 5 f f f 5 f . . . . 
+            . . . f f f f f . . f f . . . .
     `, SpriteKind.Player)
 food = sprites.create(img`
-    . . . . . . . 6 . . . . . . . . 
-    . . . . . . 8 6 6 . . . 6 8 . . 
-    . . . e e e 8 8 6 6 . 6 7 8 . . 
-    . . e 2 2 2 2 e 8 6 6 7 6 . . . 
-    . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
-    . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
-    e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
-    e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
-    e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
-    e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
-    e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
-    e 2 2 2 2 2 2 2 4 e 2 e e c . . 
-    e e 2 e 2 2 4 2 2 e e e c . . . 
-    e e e e 2 e 2 2 e e e c . . . . 
-    e e e 2 e e c e c c c . . . . . 
-    . c c c c c c c . . . . . . . . 
-    `, SpriteKind.Player)
-my_sprite.follow(food)
-my_sprite.setStayInScreen(true)
+        . . . . . . . 6 . . . . . . . . 
+            . . . . . . 8 6 6 . . . 6 8 . . 
+            . . . e e e 8 8 6 6 . 6 7 8 . . 
+            . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+            . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+            . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+            e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+            e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+            e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+            e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+            e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+            e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+            e e 2 e 2 2 4 2 2 e e e c . . . 
+            e e e e 2 e 2 2 e e e c . . . . 
+            e e e 2 e e c e c c c . . . . . 
+            . c c c c c c c . . . . . . . .
+    `, SpriteKind.Food)
+Duck.follow(food)
+Duck.setStayInScreen(true)
 food.setPosition(randint(10, 150), randint(10, 110))
 info.setScore(0)
+info.setLife(3)
+EnemyChaser()
+info.startCountdown(40)
